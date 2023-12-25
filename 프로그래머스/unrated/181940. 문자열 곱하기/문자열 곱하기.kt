426395a70bc649eabc9156fb04011d049dcf6cb6
bbangiee <139525941+21dbwls12@users.noreply.github.com>
2023-12-25 16:10:48 +0900

    [level unrated] Title: 문자열 곱하기, Time: 5.83 ms, Memory: 63.1 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181940. 문자열 곱하기/문자열 곱하기.kt
@@ -0,0 +1,9 @@
+class Solution {
+    fun solution(my_string: String, k: Int): String {
+        var answer: String = ""
+        
+        answer = my_string.repeat(k)
+        
+        return answer
+    }
+}
