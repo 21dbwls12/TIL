cf12c06ba350908506333ea1977352ac7d864f28
bbangiee <139525941+21dbwls12@users.noreply.github.com>
2023-12-27 21:07:43 +0900

    Update 문자열 곱하기.kt
    add advise

--- 프로그래머스/unrated/181940. 문자열 곱하기/문자열 곱하기.kt
+++ 프로그래머스/unrated/181940. 문자열 곱하기/문자열 곱하기.kt
@@ -7,3 +7,11 @@ class Solution {
         return answer
     }
 }
+
+"""
+.repeat() < forEach < for문
+근데 찾아보니깐 .repeat()도 안에 for문으로 이루어져있다는데 왜 직접 for문으로 로직을 짜는 것과 차이가 2배 이상나는지 모르겠음.
+.repeat()의 로직이 어떻게 구현되는지는 찾지 못함.
+
+stringBuilder는 0.05ms
+"""
