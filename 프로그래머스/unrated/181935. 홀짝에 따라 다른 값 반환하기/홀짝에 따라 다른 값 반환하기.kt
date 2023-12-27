2c84ce96e38548751f783e2d719f41cb60ac25e9
bbangiee <139525941+21dbwls12@users.noreply.github.com>
2023-12-27 21:20:40 +0900

    Update 홀짝에 따라 다른 값 반환하기.kt
    add different way and evalute

--- 프로그래머스/unrated/181935. 홀짝에 따라 다른 값 반환하기/홀짝에 따라 다른 값 반환하기.kt
+++ 프로그래머스/unrated/181935. 홀짝에 따라 다른 값 반환하기/홀짝에 따라 다른 값 반환하기.kt
@@ -15,3 +15,37 @@ class Solution {
         return answer
     }
 }
+
+"""
+최대 -> 0.39ms, 최소 -> 0.23ms
+"""
+
+class Solution {
+    fun solution(n: Int): Int {
+        var answer: Int = 0
+
+        if(n % 2 != 0) {
+            for (e in 1 .. n) {
+                if(e % 2 != 0) {
+                    answer += e
+                } else {
+                    continue
+                }
+            }
+        } else {
+            for (e in 1 .. n) {
+                if(e % 2 == 0) {
+                    answer += e * e
+                } else {
+                    continue
+                }
+            }
+        }
+
+        return answer
+    }
+}
+
+"""
+최대 -> 0.03ms, 최소 -> 0.01ms
+"""
