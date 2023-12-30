1039f8ef3f12ffe7617c8350a6ea91304ee9daa3
bbangiee <139525941+21dbwls12@users.noreply.github.com>
2023-12-30 15:23:18 +0900

    Update 마지막 두 원소.kt
    add other's solution

--- 프로그래머스/unrated/181927. 마지막 두 원소/마지막 두 원소.kt
+++ 프로그래머스/unrated/181927. 마지막 두 원소/마지막 두 원소.kt
@@ -13,3 +13,22 @@ class Solution {
         return answer
     }
 }
+
+"""
+신기한 다른 사람 풀이
+takeLast() => take 의 반대. 뒤에서 부터 n 개를 취한다
+
+class Solution {
+    fun solution(num_list: IntArray): IntArray {
+        val answer = num_list.toMutableList().apply {
+            val (first, second) = num_list.takeLast(2)
+            if (first < second) add(second - first)
+            else add(second + second)
+        }
+        return answer.toIntArray()    
+    }
+}
+
+찾아보니 take 종류가 다양함
+https://kotlinworld.com/12
+"""
