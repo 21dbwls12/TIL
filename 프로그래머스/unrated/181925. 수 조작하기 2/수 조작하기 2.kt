86f747f89c1f6060cb708dba9195add31d73984d
bbangiee <139525941+21dbwls12@users.noreply.github.com>
2023-12-31 15:04:00 +0900

    Update 수 조작하기 2.kt
    add other's solution

--- 프로그래머스/unrated/181925. 수 조작하기 2/수 조작하기 2.kt
+++ 프로그래머스/unrated/181925. 수 조작하기 2/수 조작하기 2.kt
@@ -16,3 +16,16 @@ class Solution {
         return answer.toString()
     }
 }
+
+"""
+다른 사람의 풀이
+map도 사용
+class Solution {
+    fun solution(numLog: IntArray): String {
+        val map = mapOf(1 to "w", -1 to "s", 10 to "d", -10 to "a")
+        var n = numLog[0]
+        return numLog.slice(1..numLog.lastIndex)
+            .joinToString("") { num -> map.getValue(num - n).also { n = num } }
+    }
+}
+"""
