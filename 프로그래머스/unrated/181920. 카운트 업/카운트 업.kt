18e0c6dd56be0c1cfa321841d10578051d7aff79
bbangiee <139525941+21dbwls12@users.noreply.github.com>
2024-01-01 17:20:59 +0900

    Update 카운트 업.kt
    add way that changing type

--- 프로그래머스/unrated/181920. 카운트 업/카운트 업.kt
+++ 프로그래머스/unrated/181920. 카운트 업/카운트 업.kt
@@ -16,6 +16,11 @@ class Solution {
 생각하다보니 for문을 저렇게 안넣고 answer에 바로 집어넣을 수 있지 않을까 생각함
 근데 IntArray(start_num .. end_num)이거는 IntRange로 타입이 지정됨
 전에 list는 되었던 거 같아서 그거 찾다가 아래 방법을 찾음
+
+var list = (1 .. 10).toList()
+이거는 됨... 왜 toIntArray()는 안되는지 모르겠음..
+오..? var list = (1 .. 10).toList().toIntArray() 이거 됨...
+7.ms에서 12.ms사이, 형변환을 2번이나해서 늘어난 듯
 """
 
 // 2번
