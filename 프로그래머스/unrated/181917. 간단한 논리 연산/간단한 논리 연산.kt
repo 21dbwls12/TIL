4a0dc4d8a8621938a3b15badef192a27524729b9
bbangiee <139525941+21dbwls12@users.noreply.github.com>
2024-01-02 20:42:35 +0900

    [level unrated] Title: 간단한 논리 연산, Time: 0.01 ms, Memory: 59.8 MB -BaekjoonHub

--- 프로그래머스/unrated/181917. 간단한 논리 연산/간단한 논리 연산.kt
+++ 프로그래머스/unrated/181917. 간단한 논리 연산/간단한 논리 연산.kt
@@ -1,30 +1,8 @@
 class Solution {
     fun solution(x1: Boolean, x2: Boolean, x3: Boolean, x4: Boolean): Boolean {
 
-        val answer: Boolean = down(up(x1, x2), up(x3, x4))
+        val answer: Boolean = (x1 || x2) && (x3 || x4)
 
             return answer
-    }
-    private fun up(x: Boolean, y: Boolean): Boolean {
-        return x || y
-    }
-    private fun down(x: Boolean, y: Boolean): Boolean {
-        return x && y
-    }
-}
-
-// 수정
-class Solution {
-    fun solution(x1: Boolean, x2: Boolean, x3: Boolean, x4: Boolean): Boolean {
-
-        val answer: Boolean = down(up(x1, x2), up(x3, x4))
-
-            return answer
-    }
-    private fun up(x: Boolean, y: Boolean): Boolean {
-        return x || y
-    }
-    private fun down(x: Boolean, y: Boolean): Boolean {
-        return x && y
     }
 }
