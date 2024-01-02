b6f30398b392957bc6a4c1893105c1b85bb14a28
bbangiee <139525941+21dbwls12@users.noreply.github.com>
2024-01-02 20:46:31 +0900

    Update 간단한 논리 연산.kt
    delete function

--- 프로그래머스/unrated/181917. 간단한 논리 연산/간단한 논리 연산.kt
+++ 프로그래머스/unrated/181917. 간단한 논리 연산/간단한 논리 연산.kt
@@ -1,5 +1,38 @@
 class Solution {
     fun solution(x1: Boolean, x2: Boolean, x3: Boolean, x4: Boolean): Boolean {
+        var answer: Boolean = true
+
+        answer = down(up(x1, x2), up(x3, x4))
+
+            return answer
+    }
+    fun up(x: Boolean, y: Boolean): Boolean {
+        return x || y
+    }
+    fun down(x: Boolean, y: Boolean): Boolean {
+        return x && y
+    }
+}
+
+// 수정
+class Solution {
+    fun solution(x1: Boolean, x2: Boolean, x3: Boolean, x4: Boolean): Boolean {
+
+        val answer: Boolean = down(up(x1, x2), up(x3, x4))
+
+            return answer
+    }
+    private fun up(x: Boolean, y: Boolean): Boolean {
+        return x || y
+    }
+    private fun down(x: Boolean, y: Boolean): Boolean {
+        return x && y
+    }
+}
+
+// 함수 제거
+class Solution {
+    fun solution(x1: Boolean, x2: Boolean, x3: Boolean, x4: Boolean): Boolean {
 
         val answer: Boolean = (x1 || x2) && (x3 || x4)
 
