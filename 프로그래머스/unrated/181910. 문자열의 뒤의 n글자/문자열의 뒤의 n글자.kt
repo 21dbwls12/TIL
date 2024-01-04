441751a3b8d0e7df70e9856e5a119482189302f7
bbangiee <139525941+21dbwls12@users.noreply.github.com>
2024-01-04 17:16:47 +0900

    Update 문자열의 뒤의 n글자.kt
    change answer value from variable

--- 프로그래머스/unrated/181910. 문자열의 뒤의 n글자/문자열의 뒤의 n글자.kt
+++ 프로그래머스/unrated/181910. 문자열의 뒤의 n글자/문자열의 뒤의 n글자.kt
@@ -1,6 +1,6 @@
 class Solution {
     fun solution(my_string: String, n: Int): String {
-        var answer = my_string.drop(my_string.length - n)
+        val answer = my_string.drop(my_string.length - n)
         
         return answer
     }
