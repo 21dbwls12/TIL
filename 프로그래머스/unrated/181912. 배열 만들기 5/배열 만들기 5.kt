51b64f41d8616fe7f44c5e7f6635408bf5f49ca3
bbangiee <139525941+21dbwls12@users.noreply.github.com>
2024-01-04 16:50:38 +0900

    Update 배열 만들기 5.kt
    add other solution

--- 프로그래머스/unrated/181912. 배열 만들기 5/배열 만들기 5.kt
+++ 프로그래머스/unrated/181912. 배열 만들기 5/배열 만들기 5.kt
@@ -8,3 +8,19 @@ class Solution {
         return answer.filter {it > k}
     }
 }
+
+// filter함수 사용하지 않고 조건에 맞는 숫자만 answer에 추가하는 방식
+// 메모리 용량과 처리속도를 줄일 수 있음.
+class Solution {
+    fun solution(intStrs: Array<String>, k: Int, s: Int, l: Int): MutableList<Int> {
+        val answer: MutableList<Int> = mutableListOf()
+        
+        for (num in intStrs) {
+            val n = num.substring(s, l + s).toInt()
+            if (n > k) {
+                answer.add(n)
+            }
+        }
+        return answer
+    }
+}
