042a2e3e5f14355c589c6e1f6f4579a236764ee4
bbangiee <139525941+21dbwls12@users.noreply.github.com>
2024-01-06 03:00:51 +0900

    Update 문자열의 앞의 n글자.kt
    chance answer value from variable

--- 프로그래머스/unrated/181907. 문자열의 앞의 n글자/문자열의 앞의 n글자.kt
+++ 프로그래머스/unrated/181907. 문자열의 앞의 n글자/문자열의 앞의 n글자.kt
@@ -1,6 +1,6 @@
 class Solution {
     fun solution(my_string: String, n: Int): String {
-        var answer: String = my_string.dropLast(my_string.length - n)
+        val answer: String = my_string.dropLast(my_string.length - n)
         
         return answer
     }
