03d16ac4e6de1436f1182e594ecd2aeb60fccd14
bbangiee <139525941+21dbwls12@users.noreply.github.com>
2024-01-07 17:50:19 +0900

    Update 가까운 1 찾기.kt
    change answer's init

--- 프로그래머스/unrated/181898. 가까운 1 찾기/가까운 1 찾기.kt
+++ 프로그래머스/unrated/181898. 가까운 1 찾기/가까운 1 찾기.kt
@@ -1,13 +1,11 @@
 class Solution {
     fun solution(arr: IntArray, idx: Int): Int {
-        var answer: Int = 0
+        var answer: Int = -1
         
         for (i in idx .. arr.size - 1) {
             if(arr[i] == 1) {
                 answer = i
                 break
-            } else {
-                answer = -1
             }
         }
         return answer
