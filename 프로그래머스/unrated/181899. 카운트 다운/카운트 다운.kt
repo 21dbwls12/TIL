982db3f20fbe0b3a0132c576f17bd13589e7b28f
bbangiee <139525941+21dbwls12@users.noreply.github.com>
2024-01-07 17:42:34 +0900

    [level unrated] Title: 카운트 다운, Time: 11.45 ms, Memory: 61 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181899. 카운트 다운/카운트 다운.kt
@@ -0,0 +1,7 @@
+class Solution {
+    fun solution(start: Int, end_num: Int): IntArray {
+        val answer: IntArray = (start downTo end_num).toList().toIntArray()
+        
+        return answer
+    }
+}
