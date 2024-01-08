25fbbf875ab49aee0235d67e7819668078dcbebd
bbangiee <139525941+21dbwls12@users.noreply.github.com>
2024-01-08 20:37:02 +0900

    Update 할 일 목록.kt
    add other function

--- 프로그래머스/unrated/181885. 할 일 목록/할 일 목록.kt
+++ 프로그래머스/unrated/181885. 할 일 목록/할 일 목록.kt
@@ -1,7 +1,8 @@
 class Solution {
     fun solution(todo_list: Array<String>, finished: BooleanArray): Array<String> {
-//        val todo = mapOf(todo_list to finished) -> 이렇게 하면 ket는 todo_list의 배열 전체, value는 finished의 배열 전체가 됨.
+        // val todo = mapOf(todo_list to finished) -> 이렇게 하면 ket는 todo_list의 배열 전체, value는 finished의 배열 전체가 됨.
         val todo = todo_list.zip(finished.toList()).toMap()
+        // filterValues { !it }도 똑같은 결과가 나옴.
         val answer: Array<String> = todo.filter { !it.value }.keys.toTypedArray()
         return answer
     }
