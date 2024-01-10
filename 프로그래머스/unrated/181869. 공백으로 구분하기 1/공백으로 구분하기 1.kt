82697f5bbc2b2e3f1163c6a611bbd3f5b5c7e44f
bbangiee <139525941+21dbwls12@users.noreply.github.com>
2024-01-10 16:20:21 +0900

    [level unrated] Title: 공백으로 구분하기 1, Time: 13.97 ms, Memory: 61 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181869. 공백으로 구분하기 1/공백으로 구분하기 1.kt
@@ -0,0 +1,7 @@
+class Solution {
+    fun solution(my_string: String): Array<String> {
+        var answer: Array<String> = my_string.split(" ").toTypedArray()
+        
+        return answer
+    }
+}
