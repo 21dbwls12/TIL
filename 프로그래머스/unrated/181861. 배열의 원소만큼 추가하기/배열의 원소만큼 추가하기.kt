6792a9da50ff28f44b31230a529fe1db5503a91c
bbangiee <139525941+21dbwls12@users.noreply.github.com>
2024-01-11 17:46:46 +0900

    Update 배열의 원소만큼 추가하기.kt
    add other people's solution

--- 프로그래머스/unrated/181861. 배열의 원소만큼 추가하기/배열의 원소만큼 추가하기.kt
+++ 프로그래머스/unrated/181861. 배열의 원소만큼 추가하기/배열의 원소만큼 추가하기.kt
@@ -9,3 +9,22 @@ class Solution {
         return answer
     }
 }
+
+"""
+다른 사람 풀이
+class Solution {
+    fun solution(arr: IntArray): IntArray {
+        val answer = mutableListOf<Int>()
+        arr.forEach { a -> repeat(a) { answer.add(a) } }
+        return answer.toIntArray()
+    }
+}
+
+class Solution {
+    fun solution(arr: IntArray) = mutableListOf<Int>().apply {
+        arr.forEach { num -> repeat(num) { add(num) } }
+    }
+}
+
+repeat사용하고 싶었는데 안돼서 사용못했는데 다른 사람들은 됐음..?
+"""
