f3b1c2aa6fa66d98d24590327bda01ea9cc182be
bbangiee <139525941+21dbwls12@users.noreply.github.com>
2024-01-13 07:14:34 +0900

    Update 전국 대회 선발 고사.kt
    add other people's solution

--- 프로그래머스/Lv.0/181851. 전국 대회 선발 고사/전국 대회 선발 고사.kt
+++ 프로그래머스/Lv.0/181851. 전국 대회 선발 고사/전국 대회 선발 고사.kt
@@ -10,3 +10,26 @@ class Solution {
         return answer
     }
 }
+
+// mapIndexed쓰면 됐는데... 생각 못 했음..
+"""
+다른 사람 풀이
+class Solution {
+    fun solution(rank: IntArray, attendance: BooleanArray): Int {
+        val result = rank.mapIndexed { i, v -> i to v }
+            .filter { attendance[it.first] }
+            .sortedBy { it.second }
+        return result[0].first * 10000 + result[1].first * 100 + result[2].first
+    }
+}
+
+class Solution {
+    fun solution(rank: IntArray, attendance: BooleanArray): Int {
+        val list = rank.mapIndexed { index, num -> num to index }.sortedBy { it.first }
+        return list.filter { attendance[it.second] }
+            .take(3)
+            .map { it.second }
+            .let { 10000 * it[0] + 100 * it[1] + it[2] }
+    }
+}
+"""
