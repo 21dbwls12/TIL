9825efe6e65395ead456bcab28ff4e4d51e22655
bbangiee <139525941+21dbwls12@users.noreply.github.com>
2024-01-13 18:55:19 +0900

    [level Lv.0] Title: 두 수의 합, Time: 256.09 ms, Memory: 79 MB -BaekjoonHub

---
+++ 프로그래머스/Lv.0/181846. 두 수의 합/두 수의 합.kt
@@ -0,0 +1,7 @@
+class Solution {
+    fun solution(a: String, b: String): String {
+        val answer: String = (a.toBigInteger() + b.toBigInteger()).toString()        
+
+        return answer
+    }
+}
