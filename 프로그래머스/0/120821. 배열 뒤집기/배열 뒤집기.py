3831f5764f6868ae3cb9ed3962a977cea4555420
bbangiee <139525941+21dbwls12@users.noreply.github.com>
2024-01-24 05:02:54 +0900

    Update 배열 뒤집기.py
    add friend's solution

--- 프로그래머스/0/120821. 배열 뒤집기/배열 뒤집기.py
+++ 프로그래머스/0/120821. 배열 뒤집기/배열 뒤집기.py
@@ -1,3 +1,8 @@
 def solution(num_list):
     answer = [num_list[i] for i in range(len(num_list) - 1, -1, -1)]
     return answer
+
+
+# 다른 사람 풀이
+# def solution(num_list):
+#     return num_list[::-1]
