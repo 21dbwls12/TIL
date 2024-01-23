9e76a81183965a681bbfc45986d43ce5bcaf5049
bbangiee <139525941+21dbwls12@users.noreply.github.com>
2024-01-24 04:50:04 +0900

    [level 0] Title: 문자열안에 문자열, Time: 0.00 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/0/120908. 문자열안에 문자열/문자열안에 문자열.py
+++ 프로그래머스/0/120908. 문자열안에 문자열/문자열안에 문자열.py
@@ -1,16 +1,18 @@
 def solution(str1, str2):
-    answer = 2
-    temp = ""
-    i = 0
+    answer = 1 if str2 in str1 else 2
+    
+#     answer = 2
+#     temp = ""
+#     i = 0
 
-    for s1 in str1:
-        if s1 == str2[i]:
-            temp += s1
-            i += 1
-        else:
-            temp = ""
-            i = 0
-        if temp == str2:
-            answer = 1
-            break
+#     for s1 in str1:
+#         if s1 == str2[i]:
+#             temp += s1
+#             i += 1
+#         else:
+#             temp = ""
+#             i = 0
+#         if temp == str2:
+#             answer = 1
+#             break
     return answer
