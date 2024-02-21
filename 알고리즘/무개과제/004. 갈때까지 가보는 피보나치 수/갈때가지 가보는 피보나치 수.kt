37125ea6a770f4c0c23ba497fbb0845ac80d1c10
bbangiee <139525941+21dbwls12@users.noreply.github.com>
2024-02-22 03:26:53 +0900

    Update 갈때가지 가보는 피보나치 수.kt
    add function which using iteration

--- 알고리즘/무개과제/004. 갈때까지 가보는 피보나치 수/갈때가지 가보는 피보나치 수.kt
+++ 알고리즘/무개과제/004. 갈때까지 가보는 피보나치 수/갈때가지 가보는 피보나치 수.kt
@@ -1,11 +1,10 @@
-fun fibonacci(n: Int, current: Int, nList: MutableList<Int>): Int {
-    val n = n
+fun fibonacciByRecursion(n: Int, current: Int, nList: MutableList<Long>): Long {
     var current = current
     val nList = nList
 
     if (current <= n) {
         if (current <= 2) {
-            nList.add(current - 1)
+            nList.add((current - 1).toLong())
         } else {
             val fn = nList[current - 2] + nList[current - 3]
             nList.add(fn)
@@ -18,6 +17,26 @@ fun fibonacci(n: Int, current: Int, nList: MutableList<Int>): Int {
         // ++연산자는 스코프 내에서만 유효하며 임시로 1을 증가시킴.
         // 스코프를 벗어나면 임시로 할당되었던 값은 사라지고 원래의 값으로 돌아감.
         current += 1
-        return fibonacci(n, current, nList)
+        return fibonacciByRecursion(n, current, nList)
     }
+}
+
+fun fibonacciByIteration(n: Int): Long {
+    var current = 1
+    val nList = mutableListOf<Long>()
+
+    while (current <= n) {
+        if (current <= 2) {
+            nList.add((current - 1).toLong())
+        } else {
+            val fn = nList[current - 2] + nList[current - 3]
+            nList.add(fn)
+        }
+        if (current == n) {
+            break
+        } else {
+            current++
+        }
+    }
+    return nList[n - 1]
 }
