f88b24113482c8c7e5ebd929e8d045807654ef5e
bbangiee <139525941+21dbwls12@users.noreply.github.com>
2024-04-02 01:09:16 +0900

    [Silver V] Title: 색종이, Time: 572 ms, Memory: 46820 KB -BaekjoonHub

---
+++ 백준/Silver/2563. 색종이/색종이.kt
@@ -0,0 +1,20 @@
+fun main() {
+    val dot = mutableListOf<List<Int>>()
+    val paper = readln().toInt()
+
+    repeat(paper) {
+        val (x, y) = readln().split(" ").map { it.toInt() }
+
+        for (i in x .. x + 9) {
+            for (j in y .. y + 9) {
+                val xy = listOf(i, j)
+                if (xy in dot) {
+                    continue
+                } else {
+                    dot.add(xy)
+                }
+            }
+        }
+    }
+    println(dot.size)
+}
