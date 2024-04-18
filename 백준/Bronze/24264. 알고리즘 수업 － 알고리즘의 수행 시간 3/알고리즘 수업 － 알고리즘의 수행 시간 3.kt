3b25d89af5d5a67ce454bd40d9cfa9bbcf836466
bbangiee <139525941+21dbwls12@users.noreply.github.com>
2024-04-18 18:10:02 +0900

    [Bronze III] Title: 알고리즘 수업 - 알고리즘의 수행 시간 3, Time: 92 ms, Memory: 12368 KB -BaekjoonHub

---
+++ 백준/Bronze/24264. 알고리즘 수업 － 알고리즘의 수행 시간 3/알고리즘 수업 － 알고리즘의 수행 시간 3.kt
@@ -0,0 +1,8 @@
+// 의사코드 참고 사이트 (실제 프로그래밍 언어가 아닌 알고리즘을 써놓은 코드)
+// https://ko.wikipedia.org/wiki/%EC%9D%98%EC%82%AC%EC%BD%94%EB%93%9C
+fun main() {
+    val n = readln().toLong()
+
+    println(n * n)
+    print(2)
+}
