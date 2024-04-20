495943e174c99281b3192db289a309f68226cdfe
bbangiee <139525941+21dbwls12@users.noreply.github.com>
2024-04-21 01:53:41 +0900

    Update 수학은 비대면강의입니다.kt

--- 백준/Bronze/19532. 수학은 비대면강의입니다/수학은 비대면강의입니다.kt
+++ 백준/Bronze/19532. 수학은 비대면강의입니다/수학은 비대면강의입니다.kt
@@ -86,3 +86,63 @@ fun main() {
     }
     print("$x $y")
 }
+
+// 오류 궁금증
+// 이건 왜 안되는 거지?
+// 0 2 6 1 1 7
+// println(equationA.contains(0.0)) -> true -> else문으로 들어가야함.
+// 근데 compile했을 때 if (!equationA.contains(0.0) || !equationB.contains(0.0))문 안으로 들어감..
+// if (equationA.contains(0.0) || equationB.contains(0.0))으로 바꾸고 순서만 바꿨는데 제대로 작동이 됨..
+// ????
+fun main() {
+    val (listA, listB) = readln().split(" ").map { it.toDouble() }.chunked(3)
+    val equationA = listA.toMutableList()
+    val equationB = listB.toMutableList()
+    val ax = equationA[0]
+    val bx = equationB[0]
+    var degreeY = 0.0
+    var temp = 0.0
+    var x = 0.0
+    var y = 0.0
+
+    if (!equationA.contains(0.0) || !equationB.contains(0.0)) {
+        println(1)
+        equationA.forEachIndexed { i, _ ->
+            equationA[i] *= bx
+        }
+        equationB.forEachIndexed { i, _ ->
+            equationB[i] *= ax
+        }
+        degreeY = equationA[1] - equationB[1]
+        temp = equationA[2] - equationB[2]
+        y = if (degreeY == 0.0 || temp == 0.0) {
+            0.0
+        } else {
+            temp / degreeY
+        }
+        x = equation1(listA, y)
+    } else {
+        println(2)
+        when {
+            equationA[0] == 0.0 -> {
+                y = equation0(equationA[1], equationA[2])
+                x = equation1(listB, y)
+            }
+            equationA[1] == 0.0 -> {
+                x = equation0(equationA[0], equationA[2])
+                y = equation1(listB, x)
+            }
+            equationB[0] == 0.0 -> {
+                y = equation0(equationB[1], equationB[2])
+                x = equation1(listA, y)
+            }
+            equationB[1] == 0.0 -> {
+                x = equation0(equationB[0], equationB[2])
+                y = equation1(listA, x)
+            }
+        }
+    }
+    x = round(x)
+    y = round(y)
+    print("${x.toInt()} ${y.toInt()}")
+}
